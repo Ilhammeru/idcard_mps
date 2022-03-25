439361f1abf9c20929c4cb3f0798db4a97875c7e
Ilhammeru <gumilang.dev@gmail.com>
2022-03-25 15:01:48 +0700

    init

---
+++ app.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="./style.css">
+    <!-- font -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Barlow:ital,wght@0,200;0,300;1,200&display=swap" rel="stylesheet">
+</head>
+<body>
+    <div class="hms_card">
+        <div class="hms_card_arch">
+            <img src="./assets/Asset 7.png" alt="" class="arch_1">
+            <img src="./assets/Asset 6.png" alt="" class="arch_2">
+            <img src="./assets/Asset 2.png" alt="" class="arch_3">
+            <img src="./assets/Asset 8.png" alt="" class="arch_4">
+
+            <div class="identity_box">
+                <!-- user img -->
+                <div class="user_img">
+                    <img src="./assets/person.jpg" alt="">
+                </div>
+                <p class="user_name">
+                    NAME SURENAME
+                </p>
+                <p class="user_title">
+                    ART DIRECTOR
+                </p>
+                
+            </div>
+            <div class="id_box">
+                <div class="qrcode">
+                    <img src="./assets/qrcode.png" alt="">
+                </div>
+                <div class="id_text">
+                    <p class="user_id">
+                        ID: 08080808080
+                    </p>
+                    <p class="user_valid">
+                        VALID THRU 2021
+                    </p>
+                </div>
+                <!-- <img src="./assets/barcode.jpg" alt="" class="user_code"> -->
+            </div>
+        </div>
+    </div>
+</body>
+</html>
